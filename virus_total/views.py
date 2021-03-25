0d9da0b75c98a74cbe352f4e4cfc729325336398
jm <joshua.mcshannon@gmail.com>
2021-03-24 23:04:02 -0500

    fixed time out problem kinda

--- virus_total/views.py
+++ virus_total/views.py
@@ -1,5 +1,6 @@
 import requests
 import json
+import time
 from django.shortcuts import render, redirect
 from django.core.files.storage import FileSystemStorage
 from django.utils.datastructures import MultiValueDictKeyError
@@ -30,6 +31,7 @@ def virus_total_home(request):
             response = requests.post(url, files=to_pass, params=params)
             results = response.json()
             resource_id = results['resource']
+            time.sleep(60)
             url2 = 'https://www.virustotal.com/vtapi/v2/file/report'
             param2 = {'apikey': apikey, 'resource': resource_id}
             response2 = requests.get(url2, params=param2).json()
@@ -37,11 +39,21 @@ def virus_total_home(request):
             detection = []
 
 
-            for key in response2['scans'].items():
-                av_list.append(key[0])
-            for vendor in av_list:
-                detection.append(response2['scans'][vendor]['detected'])
-            result_dict = {av_list[i]: detection[i] for i in range(len(av_list))}
-            return render(request, 'virus_total/success.html', {'result_dict': result_dict})
+
+            try:
+                for key in response2['scans'].items():
+                    av_list.append(key[0])
+                for vendor in av_list:
+                    detection.append(response2['scans'][vendor]['detected'])
+                result_dict = {av_list[i]: detection[i] for i in range(len(av_list))}
+                return render(request, 'virus_total/success.html', {'result_dict': result_dict})
+            except (KeyError):
+                time.sleep(60)
+                for key in response2['scans'].items():
+                    av_list.append(key[0])
+                for vendor in av_list:
+                    detection.append(response2['scans'][vendor]['detected'])
+                result_dict = {av_list[i]: detection[i] for i in range(len(av_list))}
+                return render(request, 'virus_total/success.html', {'result_dict': result_dict})
         except (FileNotFoundError, MultiValueDictKeyError):
-            return render(request, 'virus_total/home.html', {'error': 'file not found, is it empty?'})
+                return render(request, 'virus_total/home.html', {'error': 'file not found, is it empty?'})
